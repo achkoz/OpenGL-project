61c0862fe8baf0caf7453b766fc2ae5f9b3bba8e
achkoz <zhunusovasyl03@gmail.com>
2022-09-01 23:38:24 -0600

    OpenGL-Project

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/thef/Desktop/cppPro/OpenGL project
+# Build directory: /home/thef/Desktop/cppPro/OpenGL project/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
